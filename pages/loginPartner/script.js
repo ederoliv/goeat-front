ce508242bc28c3a69c0807c8d9a772108a384122
Eder Oliveira <eder.oliveira@progas.com.br>
2024-09-04 20:21:33 -0300

    feat: add page meusProdutos

--- pages/loginPartner/script.js
+++ pages/loginPartner/script.js
@@ -1,1 +1,8 @@
 
+function doRequest() {
+  const xhr = new XMLHttpRequest();
+xhr.open("GET", "http://localhost:8080/partners/login?email=johndoe@example.com&password=123456");
+}
+//http://localhost:8080/partners/login?email=johndoe@example.com&password=123456
+
+
--- pages/registerPartner/script.js
+++ pages/registerPartner/script.js
@@ -1,1 +1,23 @@
 
+document.getElementById('partner-login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que o formulário seja submetido normalmente
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const url = `http://localhost:8080/partners/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+
+    // Fazendo a requisição GET
+    fetch(url, {
+        method: 'GET',
+    })
+    .then(response => response.json()) // Assumindo que a resposta da API seja em JSON
+    .then(data => {
+        console.log(data); // Aqui você pode manipular a resposta da API
+        // Por exemplo, redirecionar o usuário para outra página ou mostrar uma mensagem
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        // Aqui você pode tratar erros, como mostrar uma mensagem de erro ao usuário
+    });
+});
