88a0c7d5c19cbb018f6333ba99a541630c116b9b
Eder Oliveira <eder.oliveira@progas.com.br>
2024-09-18 16:30:36 -0300

    configurando loginPartner

--- pages/loginPartner/script.js
+++ pages/loginPartner/script.js
@@ -1,8 +1,29 @@
+function login() {
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
 
-function doRequest() {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
   const xhr = new XMLHttpRequest();
-xhr.open("GET", "http://localhost:8080/partners/login?email=johndoe@example.com&password=123456");
+  xhr.open('POST', 'http://localhost:8080/partners/login', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+
+  const credentials = {
+    email: email,
+    password: password
+  };
+
+  xhr.send(JSON.stringify(credentials));
+
+  xhr.onload = function() {//em caso de sucesso
+    if (xhr.status === 200) {
+
+
+      window.location.replace('../partner/acompanhar/index.html');
+    } else {
+      console.log('Login failed!');
+      // Handle error scenario
+    }
+  };
 }
-//http://localhost:8080/partners/login?email=johndoe@example.com&password=123456
-
-
