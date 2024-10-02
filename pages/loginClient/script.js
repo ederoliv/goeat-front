6ecd6816148adf51e86872bbe733c223b1741cc4
Eder Oliveira <eder.oliveira@progas.com.br>
2024-10-02 01:28:04 -0300

    feat: loginClient route working!

--- pages/loginClient/script.js
+++ pages/loginClient/script.js
@@ -19,18 +19,14 @@ function login() {
     xhr.onload = function() {//em caso de sucesso
       if (xhr.status === 200) {
   
-      const partnerData = JSON.parse(xhr.responseText);
-      sessionStorage.setItem('userData', JSON.stringify(partnerData));
+      const clientData = JSON.parse(xhr.responseText);
+      sessionStorage.setItem('userData', JSON.stringify(clientData));
   
-      window.location.replace('../partner/acompanhar/index.html');
+      window.location.replace('../client/index.html');
       
       } else {
-        console.log('Login failed!');
+        alert('Login failed!');
         // Handle error scenario
       }
     };
-  }
-
-  function register() {
-    console.log('Função registrar client');
   }
