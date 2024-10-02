242a7372205d624d2f682f1fdcde916624e71ed2
Eder Oliveira <eder.oliveira@progas.com.br>
2024-10-02 15:57:07 -0300

    adicionarProduto melhorias

---
+++ pages/partner/meuProdutos/script.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+  const userDataString = sessionStorage.getItem('userData');
+if (userDataString) {
+    const userData = JSON.parse(userDataString);
+    console.log(userData.name); // Acessando o nome
+    console.log(userData.id); // Acessando o ID
+    document.getElementById('userName').textContent = userData.name;
+}
+};
+
+// Sidebar Toggle Codes;
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+var sidebarCloseIcon = document.getElementById("sidebarIcon");
+
+function toggleSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar_responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar_responsive");
+    sidebarOpen = false;
+  }
+}
