1085fc9f3abac8b49898144771175cad9390c3af
Eder Oliveira <eder.oliveira@progas.com.br>
2024-10-01 22:40:18 -0300

    Update partnerRegister page em script

--- pages/loginClient/script.js
+++ pages/loginClient/script.js
@@ -1,1 +1,36 @@
+function login() {
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+  
+    const email = emailInput.value;
+    const password = passwordInput.value;
+  
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:8080/clients/login', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+  
+    const credentials = {
+      email: email,
+      password: password
+    };
+  
+    xhr.send(JSON.stringify(credentials));
+  
+    xhr.onload = function() {//em caso de sucesso
+      if (xhr.status === 200) {
+  
+      const partnerData = JSON.parse(xhr.responseText);
+      sessionStorage.setItem('userData', JSON.stringify(partnerData));
+  
+      window.location.replace('../partner/acompanhar/index.html');
+      
+      } else {
+        console.log('Login failed!');
+        // Handle error scenario
+      }
+    };
+  }
 
+  function register() {
+    console.log('Função registrar client');
+  }
--- pages/registerPartner/script.js
+++ pages/registerPartner/script.js
@@ -1,23 +1,38 @@
 
-document.getElementById('partner-login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que o formulário seja submetido normalmente
+function register() {
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
 
-    const url = `http://localhost:8080/partners/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+    const name = document.getElementById('name');
+    const cnpj = document.getElementById('cnpj');
+    const number = document.getElementById('number');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirmPassword');
+  
+  
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:8080/partners/register', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+  
+    const registry = {
 
-    // Fazendo a requisição GET
-    fetch(url, {
-        method: 'GET',
-    })
-    .then(response => response.json()) // Assumindo que a resposta da API seja em JSON
-    .then(data => {
-        console.log(data); // Aqui você pode manipular a resposta da API
-        // Por exemplo, redirecionar o usuário para outra página ou mostrar uma mensagem
-    })
-    .catch(error => {
-        console.error('Erro:', error);
-        // Aqui você pode tratar erros, como mostrar uma mensagem de erro ao usuário
-    });
-});
+      email: email,
+      password: password
+    };
+  
+    xhr.send(JSON.stringify(registry));
+  
+    xhr.onload = function() {//em caso de sucesso
+      if (xhr.status === 200) {
+  
+      const partnerData = JSON.parse(xhr.responseText);
+      sessionStorage.setItem('userData', JSON.stringify(partnerData));
+  
+      window.location.replace('../loginPartner/index.html');
+      
+      } else {
+        alert('Login failed!');
+        // Handle error scenario
+      }
+    };
+  }
