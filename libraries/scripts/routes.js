fb2624ad14eb66b4d73c5c5aeda3852549d1f64f
Eder Oliveira <ederoliveira152@gmail.com>
2025-03-11 22:46:50 -0300

    Update routes.js

--- libraries/scripts/routes.js
+++ libraries/scripts/routes.js
@@ -1,15 +1,14 @@
 const root = window.location.origin;
 
 let API_BASE_URL;
-
-alert("OLA" + window.location.hostname);
 if (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') {
-    API_BASE_URL = 'http://localhost:8080/api/v1'; // Local
+    API_BASE_URL = 'http://localhost:8080/api/v1';
+    console.log("Tá local") // Local
 } else {
     API_BASE_URL = process.env.API_PROD_URL || 'https://goeat-api.ederoliv.com.br/api/v1'; // Vercel
+    console.log("Tá na nuvem")
 }
 
-alert('API Base URL:', API_BASE_URL);
 
 
 //rotas (Ah VÁ?)
